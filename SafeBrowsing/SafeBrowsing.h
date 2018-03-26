b06a23bc70445fbf9e0af20a261029f82b2e356f
Alex Rupérez <alexruperez@gmail.com>
2018-03-26 16:34:40 +0200

    Initial Commit

---
+++ SafeBrowsing/SafeBrowsing.h
@@ -0,0 +1,19 @@
+//
+//  SafeBrowsing.h
+//  SafeBrowsing
+//
+//  Created by Alex Rupérez on 26/3/18.
+//  Copyright © 2018 alexruperez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SafeBrowsing.
+FOUNDATION_EXPORT double SafeBrowsingVersionNumber;
+
+//! Project version string for SafeBrowsing.
+FOUNDATION_EXPORT const unsigned char SafeBrowsingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SafeBrowsing/PublicHeader.h>
+
+
