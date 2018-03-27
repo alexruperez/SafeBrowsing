0e4294fbfd432f02976e2576eb98c2bdc69b1aac
Alex Rupérez <alexruperez@gmail.com>
2018-03-28 01:33:08 +0200

    Tested.

--- SafeBrowsing/SafeBrowsingModels.swift
+++ SafeBrowsing/SafeBrowsingModels.swift
@@ -56,57 +56,112 @@ public enum ThreatEntryType: String, Codable {
     case executable = "EXECUTABLE"
 }
 
-public struct ErrorDetail: Codable {
+public struct ErrorDetail: Codable, Equatable {
     public let status: String
     public let message: String
     public let code: Int
+
+    public static func ==(lhs: ErrorDetail, rhs: ErrorDetail) -> Bool {
+        return lhs.status == rhs.status &&
+            lhs.message == rhs.message &&
+            lhs.code == rhs.code
+    }
 }
 
-public struct ThreatMatch: Codable {
+public struct ThreatMatch: Codable, Equatable {
     public let threatType: ThreatType
     public let platformType: PlatformType
     public let threatEntryType: ThreatEntryType
     public let threat: ThreatEntry
     public let threatEntryMetadata: ThreatEntryMetadata?
     public let cacheDuration: String
+
+    public static func ==(lhs: ThreatMatch, rhs: ThreatMatch) -> Bool {
+        return lhs.threatType == rhs.threatType &&
+            lhs.platformType == rhs.platformType &&
+            lhs.threatEntryType == rhs.threatEntryType &&
+            lhs.threat == rhs.threat &&
+            lhs.threatEntryMetadata == rhs.threatEntryMetadata &&
+            lhs.cacheDuration == rhs.cacheDuration
+    }
 }
 
-public struct ThreatEntry: Codable {
+public struct ThreatEntry: Codable, Equatable {
     public let hash: String?
     public let url: String?
     public let digest: String?
+
+    public static func ==(lhs: ThreatEntry, rhs: ThreatEntry) -> Bool {
+        return lhs.hash == rhs.hash &&
+            lhs.url == rhs.url &&
+            lhs.digest == rhs.digest
+    }
 }
 
-public struct ThreatEntryMetadata: Codable {
+public struct ThreatEntryMetadata: Codable, Equatable {
     public let entries: [MetadataEntry]
+
+    public static func ==(lhs: ThreatEntryMetadata, rhs: ThreatEntryMetadata) -> Bool {
+        return lhs.entries == rhs.entries
+    }
 }
 
-public struct MetadataEntry: Codable {
+public struct MetadataEntry: Codable, Equatable {
     public let key: String
     public let value: String
+
+    public static func ==(lhs: MetadataEntry, rhs: MetadataEntry) -> Bool {
+        return lhs.key == rhs.key &&
+            lhs.key == rhs.key
+    }
 }
 
-struct ThreatMatchesRequest: Codable {
+struct ThreatMatchesRequest: Codable, Equatable {
     let client: ClientInfo
     let threatInfo: ThreatInfo
+
+    static func ==(lhs: ThreatMatchesRequest, rhs: ThreatMatchesRequest) -> Bool {
+        return lhs.client == rhs.client &&
+            lhs.threatInfo == rhs.threatInfo
+    }
 }
 
-struct ClientInfo: Codable {
+struct ClientInfo: Codable, Equatable {
     let clientId: String
     let clientVersion: String
+
+    static func ==(lhs: ClientInfo, rhs: ClientInfo) -> Bool {
+        return lhs.clientId == rhs.clientId &&
+            lhs.clientVersion == rhs.clientVersion
+    }
 }
 
-struct ThreatInfo: Codable {
+struct ThreatInfo: Codable, Equatable {
     let threatTypes: [ThreatType]
     let platformTypes: [PlatformType]
     let threatEntryTypes: [ThreatEntryType]
     let threatEntries: [ThreatEntry]
+
+    static func ==(lhs: ThreatInfo, rhs: ThreatInfo) -> Bool {
+        return lhs.threatTypes == rhs.threatTypes &&
+            lhs.platformTypes == rhs.platformTypes &&
+            lhs.threatEntryTypes == rhs.threatEntryTypes &&
+            lhs.threatEntries == rhs.threatEntries
+    }
 }
 
-struct ErrorResponse: Codable {
+struct ErrorResponse: Codable, Equatable {
     let error: ErrorDetail
+
+    static func ==(lhs: ErrorResponse, rhs: ErrorResponse) -> Bool {
+        return lhs.error == rhs.error
+    }
 }
 
-struct ThreatMatches: Codable {
+struct ThreatMatches: Codable, Equatable {
     let matches: [ThreatMatch]
+
+    static func ==(lhs: ThreatMatches, rhs: ThreatMatches) -> Bool {
+        return lhs.matches == rhs.matches
+    }
 }
---
+++ SafeBrowsingTests/SafeBrowsingTests.swift
@@ -0,0 +1,90 @@
+//
+//  SafeBrowsingTests.swift
+//  SafeBrowsingTests
+//
+//  Created by Alex Rupérez on 28/3/18.
+//  Copyright © 2018 alexruperez. All rights reserved.
+//
+
+import XCTest
+@testable import SafeBrowsing
+
+class SafeBrowsingTests: XCTestCase {
+    
+    func testThreatMatchesRequest() {
+        SafeBrowsing.apiKey = "API_KEY"
+        let threatTypes: [ThreatType] = [.malware, .socialEngineering, .unwantedSoftware, .potenciallyHarmfulApplication]
+        let platformTypes: [PlatformType] = [.any]
+        let threatEntryTypes: [ThreatEntryType] = [.url, .executable]
+        let clientInfo = ClientInfo(clientId: "clientId", clientVersion: "clientVersion")
+        let threatEntry = ThreatEntry(hash: nil, url: "http://google.com", digest: nil)
+        let threatInfo = ThreatInfo(threatTypes: threatTypes, platformTypes: platformTypes, threatEntryTypes: threatEntryTypes, threatEntries: [threatEntry])
+        let threatMatchesRequest = ThreatMatchesRequest(client: clientInfo, threatInfo: threatInfo)
+        let request = try! SafeBrowsing.request(threatMatchesRequest)
+        let decodedThreatMatchesRequest = try! JSONDecoder().decode(ThreatMatchesRequest.self, from: request.httpBody!)
+        XCTAssertEqual(request.url?.absoluteString, "https://safebrowsing.googleapis.com/v4/threatMatches:find?key=API_KEY")
+        XCTAssertEqual(request.httpMethod, "POST")
+        XCTAssertEqual(request.value(forHTTPHeaderField: "Content-Type"), "application/json")
+        XCTAssertEqual(threatMatchesRequest, decodedThreatMatchesRequest)
+    }
+
+    func testThreatMatches() {
+        let threatEntry = ThreatEntry(hash: nil, url: "http://google.com", digest: nil)
+        let metadataEntry = MetadataEntry(key: "key", value: "value")
+        let threatEntryMetadata = ThreatEntryMetadata(entries: [metadataEntry])
+        let threatMatch = ThreatMatch(threatType: ThreatType.malware, platformType: PlatformType.any, threatEntryType: ThreatEntryType.url, threat: threatEntry, threatEntryMetadata: threatEntryMetadata, cacheDuration: "300s")
+        let threatMatches = ThreatMatches(matches: [threatMatch])
+        let encodedThreatMatches = try! JSONEncoder().encode(threatMatches)
+        let decodedThreatMatches = try! JSONDecoder().decode(ThreatMatches.self, from: encodedThreatMatches)
+        XCTAssertEqual(threatMatches, decodedThreatMatches)
+    }
+
+    func testErrorResponse() {
+        let errorDetail = ErrorDetail(status: "status", message: "message", code: 0)
+        let errorResponse = ErrorResponse(error: errorDetail)
+        let encodedErrorResponse = try! JSONEncoder().encode(errorResponse)
+        let decodedErrorResponse = try! JSONDecoder().decode(ErrorResponse.self, from: encodedErrorResponse)
+        XCTAssertEqual(errorResponse, decodedErrorResponse)
+    }
+
+    func testSafeBrowsingAPIError() {
+        let errorDetail = ErrorDetail(status: "status", message: "message", code: 0)
+        let apiError = SafeBrowsingError.api(detail: errorDetail)
+        XCTAssertEqual(apiError.debugDescription, "message")
+    }
+
+    func testSafeBrowsingThreatError() {
+        let threatEntry = ThreatEntry(hash: nil, url: "http://google.com", digest: nil)
+        let metadataEntry = MetadataEntry(key: "key", value: "value")
+        let threatEntryMetadata = ThreatEntryMetadata(entries: [metadataEntry])
+        let threatMatch = ThreatMatch(threatType: ThreatType.malware, platformType: PlatformType.any, threatEntryType: ThreatEntryType.url, threat: threatEntry, threatEntryMetadata: threatEntryMetadata, cacheDuration: "300s")
+        let threatError = SafeBrowsingError.threat(matches: [threatMatch])
+        XCTAssertEqual(threatError.debugDescription, String(describing: [threatMatch]))
+    }
+
+    func testSafeBrowsingEncodingError() {
+        let error = EncodingError.invalidValue("value", EncodingError.Context(codingPath: [], debugDescription: "debugDescription"))
+        let encodingError = SafeBrowsingError.encoding(error: error)
+        XCTAssertEqual(encodingError.debugDescription, "The data couldn’t be written because it isn’t in the correct format.")
+    }
+
+    func testSafeBrowsingUnknownError() {
+        let error = CocoaError(.featureUnsupported)
+        let unknownError = SafeBrowsingError.unknown(error: error)
+        XCTAssertEqual(unknownError.debugDescription, "The requested operation couldn’t be completed because the feature is not supported.")
+    }
+
+    func testSafeBrowsingDefaultError() {
+        let defaultError = SafeBrowsingError.unknown(error: nil)
+        XCTAssertEqual(defaultError.debugDescription, "Unknown error.")
+    }
+
+    func testIsSafe() {
+        do {
+            _ = try UIApplication.shared.isSafe(URL(string: "http://google.com")!)
+        } catch {
+            XCTAssertEqual((error as! SafeBrowsingError).debugDescription, "Get your key from https://console.cloud.google.com/apis/credentials and set SafeBrowsing.apiKey = \"YOUR_API_KEY_HERE\".")
+        }
+    }
+    
+}
